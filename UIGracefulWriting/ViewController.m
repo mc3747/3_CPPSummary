dbaa310875b9980465a90f2cbe63c689be417ca2
mc3747 <gjfax@macheng.local>
2018-07-22 19:04:25 +0800

    180722-添加UI控件

--- UIGracefulWriting/ViewController.m
+++ UIGracefulWriting/ViewController.m
@@ -7,8 +7,20 @@
 //
 
 #import "ViewController.h"
+#import "CustomBannerScrollView.h"
+#import "BannerDemoVC.h"
+#import "SDViewController.h"
+
+#import "LeftViewController.h"
+#import "RightViewController.h"
+#import "SWRevealViewController.h"
+#import "Demo1ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *btn1;
+@property (weak, nonatomic) IBOutlet UIButton *btn2;
+@property (weak, nonatomic) IBOutlet UIButton *btn3;
+@property (weak, nonatomic) IBOutlet UIButton *btn4;
 
 @end
 
@@ -16,14 +28,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSArray *buttonArray = @[_btn1,_btn2,_btn3,_btn4];
+    [buttonArray enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        UIButton *button = (UIButton*)obj;
+        NSString *selName = [NSString stringWithFormat:@"test%li",idx + 1];
+        SEL sel = NSSelectorFromString(selName);
+        [button addTarget:self action:sel forControlEvents:UIControlEventTouchUpInside];
+    }];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark -   普通滚动图片
+- (void)test1 {
+    CustomBannerScrollView *bannerView = [[CustomBannerScrollView alloc] initWithFrame:CGRectMake(0, 100, [UIScreen mainScreen].bounds.size.width, 250) ];
+    [bannerView configPictures:@[@"home_ad_1",@"home_ad_2",@"home_ad_3"]];
+    [self.view addSubview:bannerView];
 }
 
+#pragma mark -  循环滚动
+- (void)test2 {
+    SDViewController *vc = [[SDViewController alloc] init];
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
+#pragma mark -  复杂滚动
+- (void)test3 {
+    BannerDemoVC *vc = [[BannerDemoVC alloc] init];
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
+#pragma mark -  collectionView
+- (void)test4 {
+    
+    //左侧菜单栏
+    LeftViewController *leftViewController = [[LeftViewController alloc] init];
+    
+    //首页
+    Demo1ViewController *centerView1Controller = [[Demo1ViewController alloc] init];
+    
+    //右侧菜单栏
+    RightViewController *rightViewController = [[RightViewController alloc] init];
+    
+    SWRevealViewController *revealViewController = [[SWRevealViewController alloc] initWithRearViewController:leftViewController frontViewController:centerView1Controller];
+    revealViewController.rightViewController = rightViewController;
+    
+    //浮动层离左边距的宽度
+    revealViewController.rearViewRevealWidth = 230;
+    //    revealViewController.rightViewRevealWidth = 230;
+    
+    //是否让浮动层弹回原位
+    //mainRevealController.bounceBackOnOverdraw = NO;
+    [revealViewController setFrontViewPosition:FrontViewPositionLeft animated:YES];
+    
+    [self.navigationController pushViewController:revealViewController animated:YES];
+}
 
 @end
