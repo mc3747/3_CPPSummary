10d42495b28df393c147ba4f57b9d027bae857a3
mc3747 <mc3747@126.com>
2018-09-27 18:07:15 +0800

    图片资源添加

--- UIGracefulWriting/2，UI轮子/#22_跳转过渡页/Test2ViewController.m
+++ UIGracefulWriting/2，UI轮子/#22_跳转过渡页/Test2ViewController.m
@@ -10,32 +10,76 @@
 
 @interface Test2ViewController ()
 @property (nonatomic, weak) UIView *maskView;
+@property (nonatomic, weak) UILabel *countDownLabel;
+@property (nonatomic, strong) NSTimer *timer;
+@property (nonatomic, assign) NSInteger restTime;
 @end
 
 @implementation Test2ViewController
 - (void)loadView {
     [super loadView];
-    UIView *maskView3 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
-    maskView3.backgroundColor = [UIColor redColor];
-    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
-    btn.frame = CGRectMake(100, 100, 50, 50);
-    [btn setBackgroundColor:LBRandomColor forState:UIControlStateNormal];
-    [maskView3 addSubview:btn];
-    [self.view addSubview:maskView3];
-    [self.view bringSubviewToFront:maskView3];
+    UIView *maskView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+    maskView.backgroundColor = [UIColor whiteColor];
+    [self.view bringSubviewToFront:maskView];
      self.navigationController.navigationBar.hidden = YES;
-
-    [self performSelector:@selector(dismiss:) withObject:maskView3 afterDelay:3.f];
-    _maskView = maskView3;
+    [self addTopElements:maskView];
+    [self addCenterElements:maskView];
+    self.restTime = 2.f;
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.f target:self selector:@selector(updateCountDownDisplay) userInfo:nil repeats:YES];
+    [self.view addSubview:maskView];
+     _maskView = maskView;
 }
 
+#pragma mark -  添加顶部界面元素
+- (void)addTopElements:(UIView *)maskView {
+    
+    //背景
+    UIView *topView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, NAVIGATION_BAR_HEIGHT)];
+    topView.backgroundColor = Wonderful_GreenColor3;
+    [maskView addSubview:topView];
+    
+    //返回箭头
+    UIButton *backButton = [[UIButton alloc] initWithFrame:CGRectMake(6, STATUS_BAR_HEIGHT + 2, 40, 40)];
+    [backButton setImage:[UIImage imageNamed:@"loan_close"] forState:UIControlStateNormal];
+    [backButton setImage:[UIImage imageNamed:@"loan_close_down"] forState:UIControlStateHighlighted];
+    [backButton addTarget:self action:@selector(pop) forControlEvents:UIControlEventTouchUpInside];
+    [maskView addSubview:backButton];
+    
+    //中心title
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(40, + STATUS_BAR_HEIGHT, SCREEN_WIDTH - 80, NAVIGATION_BAR_HEIGHT - STATUS_BAR_HEIGHT)];
+    label.font = [UIFont boldSystemFontOfSize:18];
+    label.textColor = [UIColor blackColor];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.text = @"广金服";
+    [maskView addSubview:label];
+}
+
+#pragma mark -  添加中间元素
+- (void)addCenterElements:(UIView *)maskView {
+    
+    //logo
+    UIImageView *logoImageView = [[UIImageView alloc] init];
+    logoImageView.image = [UIImage imageNamed:@"gjf_logo"];
+    CGFloat logoWidth = (SCREEN_WIDTH / 375) * 162.f;
+    CGFloat logoHeight = (SCREEN_HEIGHT / 667) * 45.f;
+    logoImageView.frame = CGRectMake(maskView.center.x - logoWidth * 0.5f, maskView.center.y - logoHeight, logoWidth, logoHeight);
+    [maskView addSubview:logoImageView];
+    
+    //倒计时label
+    UILabel *countDownLabel = [[UILabel alloc] init];
+    countDownLabel.textAlignment = NSTextAlignmentCenter;
+    countDownLabel.textColor = [UIColor blackColor];
+    countDownLabel.font = [UIFont systemFontOfSize:16];
+    countDownLabel.text = @"即将进入广金服 2s...";
+    countDownLabel.frame = CGRectMake(0, maskView.center.y , SCREEN_WIDTH, 30);
+    [maskView addSubview:countDownLabel];
+    _countDownLabel = countDownLabel;
+}
+
+#pragma mark -  消失
 - (void)dismiss:(UIView *)view {
     GJWeakSelf;
-//    [UIView animateWithDuration:2.0 animations:^{
-//        weakSelf.navigationController.navigationBar.hidden = NO;
-//        view.alpha = 0.f;
-//        [view removeFromSuperview];
-//    }];
+
     [UIView animateWithDuration:0.5f animations:^{
         view.alpha = 0.f;
 //        view.center = CGPointMake(0, SCREEN_HEIGHT * 2.f);
@@ -46,7 +90,21 @@
         weakSelf.navigationController.navigationBar.hidden = NO;
     }];
 }
+#pragma mark -  更新倒计时显示
+- (void)updateCountDownDisplay {
+    self.restTime -= 1.f;
+    _countDownLabel.text = [NSString stringWithFormat:@"即将进入广金服 %lis...",(long)self.restTime];
+    if (self.restTime <= 0) {
+        [self dismiss:_maskView];
+        [self destroyTimer];
+    }
+}
 
+#pragma mark -  销毁定时器
+- (void)destroyTimer {
+    [self.timer invalidate];
+    self.timer = nil;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
---
+++ UIGracefulWriting/IPhoneColor.h
@@ -0,0 +1,14 @@
+//
+//  IPhoneColor.h
+//  UIGracefulWriting
+//
+//  Created by gjfax on 2018/9/27.
+//  Copyright © 2018年 macheng. All rights reserved.
+//
+
+#ifndef IPhoneColor_h
+#define IPhoneColor_h
+
+#import "UIColor+Wonderful.h"
+
+#endif /* IPhoneColor_h */
