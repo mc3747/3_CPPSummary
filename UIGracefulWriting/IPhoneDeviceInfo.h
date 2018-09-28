55bfb95733466cbeef24b54d1f1aa595c2f49029
mc3747 <mc3747@126.com>
2018-09-28 11:59:46 +0800

    手机尺寸

---
+++ UIGracefulWriting/IPhoneDeviceInfo.h
@@ -0,0 +1,24 @@
+//
+//  IPhoneDeviceInfo.h
+//  UIGracefulWriting
+//
+//  Created by gjfax on 2018/9/28.
+//  Copyright © 2018年 macheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IPhoneDeviceInfo : NSObject
+//判断是否是iphoneX系列的4种方法
++ (BOOL)isiPhoneXSerial_1;
++ (BOOL)isiPhoneXSerial_2;
++ (BOOL)isiPhoneXSerial_3;
++ (BOOL)isiPhoneXSerial_4;
+
+//根据device model来获取设备信息
++(NSString *) deviceModel;
+@end
+
+NS_ASSUME_NONNULL_END
--- UIGracefulWriting/IPhoneSize.h
+++ UIGracefulWriting/IPhoneSize.h
@@ -9,29 +9,72 @@
 #ifndef IphoneSize_h
 #define IphoneSize_h
 
+/**全屏宽高******************************/
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+
+//按照iphone6系列给图的缩放比例
+#define kScaleH      (SCREEN_HEIGHT/667.0)
+#define kScaleW      (SCREEN_WIDTH/375.0)
+
+/**常用屏幕宽高******************************/
+
+#define IS_IPHONE_Serial (IS_IPHONE && [[UIScreen mainScreen] bounds].size.height >= 812.0)
+// top安全高度
+#define Top_SafeArea_HEIGHT (IS_IPHONE_Serial ? 24.f : 0.f)
+// 状态栏高度
+#define STATUS_BAR_HEIGHT (IS_IPHONE_Serial ? 44.f : 20.f)
+// 导航栏高度
+#define NAVIGATION_BAR_HEIGHT (IS_IPHONE_Serial ? (64.f + 24.f): 64.f)
+
+// tabBar高度
+#define TAB_BAR_HEIGHT (IS_IPHONE_Serial ? (49.f+34.f) : 49.f)
+// bottom安全高度
+#define Bottom_SafeArea_HEIGHT (IS_IPHONE_Serial ? 34.f : 0.f)
+
+
 /**机型判断方法1：currentMode来判断******/
-//判断是否是ipad
+//判断是否是ipad或者iphone
 #define isPad ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
+#define is_PAD (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+#define IS_IPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+
 //判断iPhone4系列
-#define kiPhone4 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 960), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+#define kIPhone4 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 960), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
 //判断iPhone5系列
-#define kiPhone5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+#define kIPhone5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
 //判断iPhone6系列
-#define kiPhone6 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(750, 1334), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+#define kIPhone6 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(750, 1334), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
 //判断iphone6+系列
-#define kiPhone6Plus ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2208), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+#define kIPhone6_Plus ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2208), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
 //判断iPhoneX
-#define IS_IPHONE_X ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+#define kIPHONE_X ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
 //判断iPHoneXr
-#define IS_IPHONE_Xr ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(828, 1792), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+#define kIPHONE_Xr ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(828, 1792), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
 //判断iPhoneXs
-#define IS_IPHONE_Xs ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+#define kIPHONE_Xs ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
 //判断iPhoneXs Max
-#define IS_IPHONE_Xs_Max ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2688), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+#define kIPHONE_XMax ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2688), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
 
-/**机型判断方法2：bounds来判断******/
+/**机型判断方法2：bounds结合DBL_EPSILON来判断******/
+    //DBL_EPSILON和 FLT_EPSILON主要用于单精度和双精度的比较当中
+#define IS_WIDESCREEN_5    (fabs((double)[[UIScreen mainScreen] bounds].size.height - (double)568) < __DBL_EPSILON__)
 
-/**机型判断方法3：device model来判断******/
+#define IS_WIDESCREEN_6     (fabs((double)[[UIScreen mainScreen] bounds].size.height - (double)667) < __DBL_EPSILON__)
 
+#define IS_WIDESCREEN_6Plus  (fabs((double)[[UIScreen mainScreen] bounds].size.height - (double)736) < __DBL_EPSILON__)
+
+#define IS_WIDESCREEN_X  (fabs((double)[[UIScreen mainScreen] bounds].size.height - (double)812) < __DBL_EPSILON__)
+
+#define IS_WIDESCREEN_XMax  (fabs((double)[[UIScreen mainScreen] bounds].size.height - (double)896) < __DBL_EPSILON__)
+
+/**机型判断方法3：bounds的宽来判断******/
+#define IS_IPHONE_5 (IS_IPHONE && [[UIScreen mainScreen] bounds].size.height == 568.0)
+#define IS_IPHONE_6 (IS_IPHONE && [[UIScreen mainScreen] bounds].size.height == 667.0)
+#define IS_IPHONE_6Plus (IS_IPHONE && [[UIScreen mainScreen] bounds].size.height == 736.0)
+#define IS_IPHONE_X (IS_IPHONE && [[UIScreen mainScreen] bounds].size.height == 812.0)
+#define IS_IPHONE_XMax (IS_IPHONE && [[UIScreen mainScreen] bounds].size.height == 896.0)
+
+/**机型判断方法4：device model来判断******/
 
 #endif /* IphoneSize_h */
