ab54408052f9b666fdb2ea363a4a1a20f7734f5a
小马 <mc3747@126.com>
2021-05-08 17:17:17 +0800

    cpp工程

---
+++ CPPSummary/CPPSummary/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPPSummary
+//
+//  Created by kk on 2021/5/8.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
